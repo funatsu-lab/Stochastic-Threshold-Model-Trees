23cc252f6120539a665068110c65cdada7c6c98a
nusanyoma <49966285+nusanyoma@users.noreply.github.com>
2020-07-03 15:02:10 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from EnsembleTrees import __version__
 
 setup(name='EnsembleTrees',
     version=__version__,
-    description='EnsembleTrees - a tree-based algorithm for extrapolation',
+    description='Stochastic Threshold Model Tree - a tree-based algorithm for extrapolation',
     classifiers=[
         'Programming Language :: Python :: 3.6',
         'Topic :: Cheminformatics :: Extrapolation',
     ],
-    url='https://github.com/funatsu-lab/Ensemble-trees',
+    url='https://github.com/funatsu-lab/Stochastic-Threshold-Model-Tree',
     author='Kohei Numata, Kenichi Tanaka',
     author_email='knumata@chemsys.t.u-tokyo.ac.jp',
     packages=find_packages(),
-    install_requires=['numpy>=1.18'],
+    install_requires=['numpy>=1.17', 'joblib>=0.13'],
     )
