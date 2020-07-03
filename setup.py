cf938eddad6a83582f2ae8ec24d6d5f9fe9a6bfd
nusanyoma <49966285+nusanyoma@users.noreply.github.com>
2020-07-03 23:45:51 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
-from StochasticThresholdModelTree import __version__
+from StochasticThresholdModelTrees import __version__
 
-setup(name='Stochastic Threshold Model Tree',
+setup(name='Stochastic Threshold Model Trees',
     version=__version__,
-    description='Stochastic Threshold Model Tree - a tree-based algorithm for extrapolation',
+    description='Stochastic Threshold Model Trees - a tree-based algorithm for extrapolation',
     classifiers=[
         'Programming Language :: Python :: 3.7',
         'Topic :: Cheminformatics :: Extrapolation',
     ],
-    url='https://github.com/funatsu-lab/Stochastic-Threshold-Model-Tree',
+    url='https://github.com/funatsu-lab/Stochastic-Threshold-Model-Trees',
     author='Kohei Numata, Kenichi Tanaka',
     author_email='knumata@chemsys.t.u-tokyo.ac.jp',
     packages=find_packages(),
